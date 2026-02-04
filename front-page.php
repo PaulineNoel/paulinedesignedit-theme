894dcaef51239c1635e63de1014620f68a7206dc
PaulineNoel <paulinenoel@icloud.com>
2026-02-04 15:11:55 +0100

    imroving css and front page

--- front-page.php
+++ front-page.php
@@ -4,13 +4,17 @@
     <!-- HERO (hardcoded) -->
     <section class="home-hero">
       <div class="display-flex-center">
-        <div class="half-width-container">
-          <h1>I design digital experiences that actually work.</h1>
+        <div class="half-width-container hero-text">
+          <h1>I design digital experiences that <span class="highlight">actually work</span>.</h1>
           <h2>Clean interfaces, thoughtful UX, and websites built to support real business goals.</h2>
-          <p class="hero-subtitle">
+          <h3 class="hero-subtitle">
             I’m a UI/UX Designer & Web Strategist who bridges design, development, and strategy — turning complex ideas into intuitive, high-performing digital products.
             Professional, detail-driven, and yes… I enjoy the process too.
-          </p>
+          </h3>
+          <div class="display-flex two-button-section">
+            <a href="/projects" class="primary-btn btn two-button-section-element">View all projects</a>
+            <a href="/contact-me" class="secondary-btn btn two-button-section-element">View all projects</a>
+          </div>
         </div>
         
         <img class="half-width-container hero-image" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Pauline-Noel-Photo.jpg" alt="Portrait of Pauline Noël, web and UI/UX designer">
@@ -20,13 +24,15 @@
 
     <!-- PROJECTS LOOP -->
     <section class="home-projects container">
-      <h2>Selected Work</h2>
+      <h2>Selected Projects</h2>
+      <h3>A curated selection of projects where strategy, design, and execution come together.</h3>
+      <p>Each project reflects a clear business objective — whether it’s improving usability, simplifying complex information, or building scalable websites that teams can actually manage.</p>
 
       <div class="projects-grid">
           <?php get_template_part('template-parts/loop', 'project'); ?>
       </div>
 
-      <a href="/projects" class="btn">View all projects</a>
+      <a href="/projects" class="primary-btn btn">View all projects</a>
 
     </section>
 </main>
