96512e54879b64c607c169888f169f71bc8ce30d
PaulineNoel <paulinenoel@icloud.com>
2026-02-04 14:25:27 +0100

    changes to homepage, image fix etc

--- front-page.php
+++ front-page.php
@@ -1,36 +1,34 @@
 <?php get_header(); ?>
 
 <main class="home">
-
-  <!-- HERO (hardcoded) -->
-  <section class="home-hero">
-    <div class="hero">
-      <div class="hero-text">
-        <h1>I design digital experiences that actually work.</h1>
-        <h2>Clean interfaces, thoughtful UX, and websites built to support real business goals.</h2>
-        <p class="hero-subtitle">
-          I’m a UI/UX Designer & Web Strategist who bridges design, development, and strategy — turning complex ideas into intuitive, high-performing digital products.
-          Professional, detail-driven, and yes… I enjoy the process too.
-        </p>
+    <!-- HERO (hardcoded) -->
+    <section class="home-hero">
+      <div class="display-flex-center">
+        <div class="half-width-container">
+          <h1>I design digital experiences that actually work.</h1>
+          <h2>Clean interfaces, thoughtful UX, and websites built to support real business goals.</h2>
+          <p class="hero-subtitle">
+            I’m a UI/UX Designer & Web Strategist who bridges design, development, and strategy — turning complex ideas into intuitive, high-performing digital products.
+            Professional, detail-driven, and yes… I enjoy the process too.
+          </p>
+        </div>
+        
+        <img class="half-width-container hero-image" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Pauline-Noel-Photo.jpg" alt="Portrait of Pauline Noël, web and UI/UX designer">
+        
       </div>
-      <div class="hero-image">
-        <img src="http://pauline-designed-it.local/wp-content/uploads/2026/02/Pauline-Noel-scaled.jpg" alt="Portrait of Pauline Noël, web and UI/UX designer">
+    </section>
+
+    <!-- PROJECTS LOOP -->
+    <section class="home-projects container">
+      <h2>Selected Work</h2>
+
+      <div class="projects-grid">
+          <?php get_template_part('template-parts/loop', 'project'); ?>
       </div>
-    </div>
-  </section>
 
-  <!-- PROJECTS LOOP -->
-  <section class="home-projects">
-    <h2>Selected Work</h2>
+      <a href="/projects" class="btn">View all projects</a>
 
-    <div class="projects-grid">
-        <?php get_template_part('template-parts/loop', 'project'); ?>
-    </div>
-
-    <button type="button" href=""></button>
-
-  </section>
-
+    </section>
 </main>
 
 <?php get_footer(); ?>
